8ceaf83b577070881f9b92519a2af3243ef1d6aa
Perc2val <fsb099@aol.de>
2025-12-11 11:07:15 +0100

    digits can be clicked

--- javascript.js
+++ javascript.js
@@ -1,6 +1,104 @@
-let numberOne = 0;
+let numberOne = "";
 let operator = "plus";
 let numberTwo = 0;
+
+const calculatorTop = document.querySelector(".calculatorTop")
+const zero = document.querySelector(".zero")
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const p = document.createElement("p")
+calculatorTop.appendChild(p)
+
+zero.addEventListener("click", (e)=> {
+    numberOne += "0"
+    p.textContent = numberOne;
+})
+
+one.addEventListener("click", (e)=> {
+    numberOne += "1";
+    p.textContent = numberOne;
+})
+
+two.addEventListener("click", (e)=> {
+    numberOne += "2"
+    p.textContent = numberOne;
+})
+
+three.addEventListener("click", (e)=> {
+    numberOne += "3"
+    p.textContent = numberOne;
+})
+
+four.addEventListener("click", (e)=> {
+    numberOne += "4"
+    p.textContent = numberOne;
+})
+
+five.addEventListener("click", (e)=> {
+    numberOne += "5"
+    p.textContent = numberOne;
+})
+
+six.addEventListener("click", (e)=> {
+    numberOne += "6"
+    p.textContent = numberOne;
+})
+
+seven.addEventListener("click", (e)=> {
+    numberOne += "7"
+    p.textContent = numberOne;
+})
+
+eight.addEventListener("click", (e)=> {
+    numberOne += "8"
+    p.textContent = numberOne;
+})
+
+nine.addEventListener("click", (e)=> {
+    numberOne += "9"
+    p.textContent = numberOne;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function add(numberOne, numberTwo) {
     partResult = numberOne + numberTwo;
