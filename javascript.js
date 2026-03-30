a704075132c730b9fc3bfacd36dcd6662d669d49
Perc2val <fsb099@aol.de>
2026-03-30 15:02:15 +0200

    operate function done

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-
+let numberOne;
+let numberTwo;
+let operator;
 
 
 function add(numberOne, numberTwo){
@@ -15,4 +17,16 @@ function multiply(numberOne, numberTwo){
 
 function divide(numberOne, numberTwo){
     return numberOne/numberTwo;
+};
+
+function operate(numberOne, numberTwo, operator){
+    if(operator == "plus"){
+        return add(numberOne, numberTwo);
+    } else if (operator == "minus"){
+        return subtract(numberOne, numberTwo);
+    } else if (operator == "multi"){
+        return multiply(numberOne, numberTwo);
+    } else {
+        return divide(numberOne, numberTwo);
+    };
 };
